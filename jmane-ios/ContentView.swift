f0317701033f865e1e161c8bd349b9de4e9b40d9
stakeuchi1145 <shintyakos@engineer-life.com>
2025-12-03 21:09:21 +0900

    Initial Commit

---
+++ jmane-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  jmane-ios
+//
+//  Created by Shin Takeuchi on 2025/12/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ jmane-ios/jmane_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  jmane_iosApp.swift
+//  jmane-ios
+//
+//  Created by Shin Takeuchi on 2025/12/03.
+//
+
+import SwiftUI
+
+@main
+struct jmane_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
